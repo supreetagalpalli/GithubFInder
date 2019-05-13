4cbf7db709918b683b08053b22bb1cc97fd955f8
Supreeta Galpalli <supreeta.galpalli@gmail.com>
2019-05-13 10:32:24 -0400

    Initial commit

---
+++ github.js
@@ -0,0 +1,22 @@
+class Github{
+    constructor(){
+        this.client_id = '22f64bc3bc5034d09d72';
+        this.client_secret = '6b4c9cd939004e2fcb4941473578604f170e0375';
+        this.repos_count = 5;
+        this.repos_sort = 'created: asc';
+     }
+
+    async getUser(user){ 
+        const profileResponse = await fetch(`http://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+        const repoResponse = await fetch(`http://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+        const profile = await profileResponse.json();
+        const repos = await repoResponse.json();
+
+        return {
+            profile,
+            repos
+        }
+    }
+}
